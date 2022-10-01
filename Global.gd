c71f45933766bc1248d480e788b4eb9889871d84
mdisko <mdisko@iu.edu>
2022-10-01 03:39:57 -0400

    Completes the exercise

--- Global.gd
+++ Global.gd
@@ -18,6 +18,17 @@ var sway_period = 0.1
 export var default_starting_in = 4
 export var default_lives = 5
 
+var colors = [
+	Color8(224,49,49)
+	,Color8(255,146,43)
+	,Color8(255,212,59)
+	,Color8(148,216,45)
+	,Color8(34,139,230)
+	,Color8(132,94,247)
+	,Color8(190,75,219)
+	,Color8(134,142,150)
+]
+
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 	randomize()
@@ -26,7 +37,12 @@ func _ready():
 	reset()
 
 func _physics_process(_delta):
-	pass
+	if color_rotate >= 0:
+		color_rotate -= color_rotate_index
+		color_rotate_index *= 1.05
+	else:
+		color_rotate_index = 0.1
+	sway_index += sway_period
 
 func _unhandled_input(event):
 	if event.is_action_pressed("menu"):
